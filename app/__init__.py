e825dd9db37fc3df153f64d5d25229e3eb5c9964
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-05 23:29:38 +0300

    [ Feature #160248798 ] Fetch a specific order
    This commit returns a specific order when requested using the route 'api/V1/orders/<orderid>'.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 """__init__"""
-from flask import Flask
+from flask import Flask, jsonify, abort
 
 
 def create_app():
     """Starts the application"""
+    from app.models import ORDERS
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
+
+    @app.route('/api/V1/orders/<int:orderid>', methods=['GET'])
+    def get_specific_order(orderid):
+        ids = [order for order in ORDERS if order["order_id"] == orderid]
+        if not ids:
+            abort(404)
+
+        return jsonify({"Order": ids})
 
     return app
