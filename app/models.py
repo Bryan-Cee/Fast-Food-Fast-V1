23cb0241219da41f9c60722b6224e1674dae9e57
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-05 22:37:02 +0300

    Update models file

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@
 
 MENU = [{"id": 1, "foodname": "Pizza", "foodprice": "$4.99"},
         {"id": 2, "foodname": "Burger", "foodprice": "$5.99"},
-        {"id": 3, "foodname": "Pizza", "foodprice": "$6.99"}]
+        {"id": 3, "foodname": "Fries", "foodprice": "$6.99"}]
 
-ORDERS = []
+ORDERS = [{"order_id": 1, "foodname": "Pizza", "status": "accepted"},
+          {"order_id": 2, "foodname": "Burger", "status": "accepted"}]
