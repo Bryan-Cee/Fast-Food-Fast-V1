86f647a9d6117c8659fdfd9f16fbd00c9bd91754
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-06 00:22:08 +0300

    [ Feature #160248703 ] Place new order
    User can place an order through the url '/api/V1/orders'

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 """__init__"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 
 
 def create_app():
@@ -9,8 +9,21 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
 
-    @app.route('/api/V1/orders', methods=['GET'])
+    new_order = [order for order in ORDERS]
+
+    @app.route('/api/V1/orders', methods=['GET', 'POST'])
     def get_all_orders():
-        return jsonify({'Orders': ORDERS})
+        if request.method == 'POST':
+            order_details = {'order_id': new_order[-1]['order_id'] + 1,
+                             'foodname': request.json['foodname'],
+                             'status': 'new order'}
+            foodname = [food for food in MENU if order_details['foodname'] == food['foodname']]
+            if not foodname:
+                abort(404)
+
+            new_order.append(order_details)
+            return 'Order for {} has been received'.format(order_details['foodname']), 201
+
+        return jsonify({'Orders': new_order})
 
     return app
