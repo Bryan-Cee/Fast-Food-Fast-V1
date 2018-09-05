029f5d5cd2d7da30d4e2663c326be2a489b5eefa
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-06 00:47:19 +0300

    [ Feature #160248841 ] Update status of an order
    This commit updates the status of an order through the route '/api/V1/<orderid>'.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 """__init__"""
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 
 
 def create_app():
@@ -9,11 +9,15 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
 
-    @app.route('/api/V1/orders/<int:orderid>', methods=['GET'])
+    @app.route('/api/V1/orders/<int:orderid>', methods=['GET', 'PUT'])
     def get_specific_order(orderid):
         ids = [order for order in ORDERS if order["order_id"] == orderid]
         if not ids:
             abort(404)
+
+        if request.method == 'PUT':
+            ids[0]["status"] = request.json["status"]
+            return 'Order id {} has been updated'.format(ids[0]["order_id"])
 
         return jsonify({"Order": ids})
 
