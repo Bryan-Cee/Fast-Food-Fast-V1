031a3ee493994d3cc646c61f62a157303cd54e60
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-05 22:54:05 +0300

    [ Feature #160248764 ] Get all orders
    This commit gets all orders by using '/api/V1/orders' route

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 """__init__"""
-from flask import Flask
+from flask import Flask, jsonify
 
 
 def create_app():
     """Starts the application"""
+    from app.models import ORDERS, MENU
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
+
+    @app.route('/api/V1/orders', methods=['GET'])
+    def get_all_orders():
+        return jsonify({'Orders': ORDERS})
 
     return app
