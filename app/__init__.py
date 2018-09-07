3e6f1445fd7fdc563eb051740d4e0f3e6d7caac4
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-07 11:03:53 +0300

    [ Feature #160248764 ] Add logic to retrieve orders
    The models.py file is changed so this file was modified to enable it to use the data structures provided by the models.py file

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,17 @@ from flask import Flask, jsonify
 
 def create_app():
     """Starts the application"""
-    from app.models import ORDERS, MENU
+    from app.models import OrderFood
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
 
     @app.route('/api/V1/orders', methods=['GET'])
     def get_all_orders():
-        return jsonify({'Orders': ORDERS})
+        new_order = OrderFood().get_all_orders()
+        if not new_order:
+            return 'No orders yet'
+
+        return jsonify({'Orders': new_order})
 
     return app
