bf017b75a12a7047c554337a53be8c6035ad101e
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-07 11:00:28 +0300

    [ Chore #160337005 ] Enable storage of data
    This commit enables the user to store data easily

--- app/models.py
+++ app/models.py
@@ -1,8 +1,17 @@
 """Holds the dummy data for the application"""
 
-MENU = [{"id": 1, "foodname": "Pizza", "foodprice": "$4.99"},
-        {"id": 2, "foodname": "Burger", "foodprice": "$5.99"},
-        {"id": 3, "foodname": "Fries", "foodprice": "$6.99"}]
 
-ORDERS = [{"order_id": 1, "foodname": "Pizza", "status": "accepted"},
-          {"order_id": 2, "foodname": "Burger", "status": "accepted"}]
+class OrderFood:
+    """Create a new list to store user data"""
+
+    def __init__(self):
+        self.all_orders = []
+        self.menu = []
+
+    def get_all_orders(self):
+        """Create a new list to store the orders"""
+        return self.all_orders
+
+    def current_menu(self):
+        """Create a new list to store the menu items"""
+        return self.menu
