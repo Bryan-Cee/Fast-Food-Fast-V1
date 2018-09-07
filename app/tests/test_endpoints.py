fec2b9bcfb4ad5ca1cdec06cc978bc0894b30b7a
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-07 16:55:52 +0300

    [ Chore #160337016 ] Finish tests

--- app/tests/test_endpoints.py
+++ app/tests/test_endpoints.py
@@ -15,23 +15,24 @@ class TestEndpoints(TestCase):
 		self.assertEqual('No orders yet', res.get_data(as_text=True))
 
 	def test_get_menu_with_no_meals(self):
-		res = self.client.get("/api/V1/orders/menu")
+		res = self.client.get("/api/V1/menu")
 		self.assertEqual(200, res.status_code)
 		self.assertEqual('No meals have been added to the menu', res.get_data(as_text=True))
 
 	def test_adding_meal_to_menu(self):
-		res = self.client.post("/api/V1/orders/menu", json={'foodname': 'Pizza', 'price': '$4.99'})
+		res = self.client.post("/api/V1/menu", json={'foodname': 'Pizza', 'price': '$4.99'})
 		self.assertEqual(201, res.status_code)
 		self.assertEqual('Meal added', res.get_data(as_text=True))
+		print(res.get_data(as_text=True))
 
 	def test_get_menu_with_meals(self):
-		res = self.client.post("/api/V1/orders/menu", json={'foodname': 'Pizza', 'price': '$4.99'})
+		res = self.client.post("/api/V1/menu", json={'foodname': 'Pizza', 'price': '$4.99'})
 		self.assertEqual(201, res.status_code)
-		res = self.client.get("/api/V1/orders/menu")
+		res = self.client.get("/api/V1/menu")
 		self.assertEqual(200, res.status_code)
 
 	def test_get_all_order_when_orders_exist(self):
-		res = self.client.post("/api/V1/orders/menu", json={'foodname': 'Pizza', 'price': '$4.99'})
+		res = self.client.post("/api/V1/menu", json={'foodname': 'Pizza', 'price': '$4.99'})
 		self.assertEqual(201, res.status_code)
 		res = self.client.get("/api/V1/orders")
 		self.assertEqual(200, res.status_code)
