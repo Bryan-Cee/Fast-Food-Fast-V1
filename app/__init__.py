b825ddb9ac29e5b1ace46d8cb46dfa1e3324153b
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-07 14:18:40 +0300

    [ Feature #160336877 ] Adds a meal to the menu
    This commit adds a meal to the menu and prevents duplication of the entry

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 """__init__"""
-from flask import Flask
+from flask import Flask, jsonify, request
 
 
 def create_app():
@@ -7,5 +7,28 @@ def create_app():
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
+
+    from app.models import OrderFood
+
+    menu = OrderFood().current_menu()
+
+    @app.route("/api/V1/menu", methods=['POST'])
+    def add_meal_to_menu():
+        mealname = [meal[key] for meal in menu for key in meal.keys() if key == 'foodname']
+        if request.json['foodname'] not in mealname:
+            if not menu:
+                meal = {'order_id': 1,
+                        "foodname": request.json['foodname'],
+                        "price": request.json['price']}
+            else:
+                meal = {'order_id': menu[-1]['order_id'] + 1,
+                        "foodname": request.json['foodname'],
+                        "price": request.json['price']}
+
+            menu.append(meal)
+
+            return 'Meal added', 201
+
+        return 'Meal is already in the menu', 400
 
     return app
