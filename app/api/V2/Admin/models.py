56e66648c018243e6e70c2f8488362db602f39b7
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-27 15:35:04 +0300

    [ Feature #160336877 ] Add meals to menu

---
+++ app/api/V2/Admin/models.py
@@ -0,0 +1,27 @@
+from flask import jsonify, make_response
+import psycopg2
+import app.config as config
+
+class Admin:
+    def __init__(self):
+        self.conn = psycopg2.connect(host="localhost",
+                                     database=config.DBNAME,
+                                     user=config.USER,
+                                     password=config.PASSWORD)
+
+    def add_to_menu(self, meal_name, meal_desc, meal_price):
+        with self.conn as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT meal_name FROM Menu WHERE meal_name = %s", (meal_name,))
+                checks = cur.fetchone()
+                if not meal_name or not meal_price:
+                    conn.rollback()
+                    return 'Please enter the correct format of keys'
+                if checks is not None:
+                    conn.rollback()
+                    return 'The meal is already in the menu'
+
+                cur.execute("INSERT INTO Menu(meal_name, meal_desc, meal_price) VALUES (%s, %s, %s)",
+                            (meal_name, meal_desc, meal_price))
+                conn.commit()
+                return make_response(jsonify({"status": "Meal has been added to the menu"}))
---
+++ app/api/V2/Admin/views.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, request
+
+from .models import Admin
+
+admin_bp = Blueprint('admin', __name__, url_prefix='/api/v2')
+
+
+@admin_bp.route('/menu', methods=['GET', 'POST'])
+def get_all_users():
+    if request.method == 'POST':
+        data = request.get_json()
+        meal_name = data.get('meal_name')
+        meal_desc = data.get('meal_desc')
+        meal_price = data.get('meal_price')
+        return Admin().add_to_menu(meal_name, meal_desc, meal_price)
+
+    return Admin().get_the_menu()
+
