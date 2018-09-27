a018915cd23e0c488a0a4fffdd981c2eb32d2f78
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-27 15:42:43 +0300

    [ Chore #160782967 ] Add admin routes to app

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,13 @@ def create_app():
    
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
+
+    from .api.V2.database import InitDB
+    InitDB.create_tables()
+
+    from app.api.V2.Admin.views import admin_bp
+    app.register_blueprint(admin_bp)
+
     from app.api.V1.views import V1
     app.register_blueprint(V1)
     return app
