27ffef43a657b5794fa60659e0333c37f329c7a2
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-29 20:32:52 +0300

    [ Chore #160790504 ] Update tests

--- app/api/tests/V2/test_add_meal_to_menu.py
+++ app/api/tests/V2/test_add_meal_to_menu.py
@@ -5,14 +5,14 @@ class TestAddMealToMenu(MainTestCase):
 
 	def test_add_meal(self):
 		res = self.client.post('/api/v2/menu', json={"meal_name": 'Pizza', "meal_desc": 'Seasoned', "meal_price": 7.99})
-		self.assertEqual('{"status": "Meal has been added to the menu"}', res.data)
+		self.assertIn('Meal has been added to the menu', res.get_data(as_text=True))
 		self.assertEqual(201, res.status_code)
 
-		res = self.client.post('/api/v2/menu')
-		self.assertEqual('Please enter the correct format of keys', res.data)
+		res = self.client.post('/api/v2/menu', json={})
+		self.assertEqual(b'Please enter the correct format of keys', res.data)
 
 		res = self.client.post('/api/v2/menu', json={"meal_name": 'Pizza', "meal_desc": 'Seasoned', "meal_price": 7.99})
-		self.assertEqual('The meal is already in the menu', res.data)
+		self.assertEqual(b'The meal is already in the menu', res.data)
 
 	def test_get_menu(self):
 		res = self.client.get('/api/v2/menu')
