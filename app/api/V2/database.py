c51271d540b42277fb74a5853baa0615e48d7dda
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-29 16:00:39 +0300

    [ Chore #160782967 ] Modify database file

--- app/api/V2/database.py
+++ app/api/V2/database.py
@@ -51,53 +51,6 @@ class Default:
     )
 
 
-class TestDatabase:
-    """Initializes the tables in the test database"""
-
-    sql = (
-        """	
-        DROP TABLE IF EXISTS Users CASCADE;
-        CREATE TABLE IF NOT EXISTS Users (
-            user_id serial NOT NULL PRIMARY KEY,
-            username varchar(45) NOT NULL,
-            password varchar(255) NOT NULL,
-            Admin boolean DEFAULT FALSE  NOT NULL
-            );
-        """
-        ,
-        """
-        DROP TABLE IF EXISTS Orders CASCADE;
-        CREATE TABLE IF NOT EXISTS  Orders (
-            order_id serial NOT NULL PRIMARY KEY,
-            meal_id int NOT NULL,
-            time_of_order time NOT NULL,
-            user_id int NOT NULL
-        );
-        """
-        ,
-        """
-        DROP TABLE IF EXISTS Menu CASCADE;
-        CREATE TABLE IF NOT EXISTS Menu (
-            meal_id serial PRIMARY KEY NOT NULL,
-            meal_name varchar(45) NOT NULL,
-            meal_desc varchar(100)
-        );
-        """
-        ,
-        """
-        ALTER TABLE Orders
-        ADD CONSTRAINT orders_users_user_id_fk
-        FOREIGN KEY (order_id) REFERENCES Users (user_id) ON DELETE CASCADE ON UPDATE CASCADE;
-        """
-        ,
-        """
-        ALTER TABLE Orders
-        ADD CONSTRAINT orders_menu_meal_id_fk
-        FOREIGN KEY (meal_id) REFERENCES Menu (meal_id) ON DELETE CASCADE ON UPDATE CASCADE;
-        """
-    )
-
-
 class InitDB:
     def __init__(self):
         pass
@@ -105,9 +58,9 @@ class InitDB:
     @staticmethod
     def create_tables():
         conn = psycopg2.connect(host="localhost",
-                        database=config.DBNAME,
-                        user=config.USER,
-                        password=config.PASSWORD)
+                                database=config.DBNAME,
+                                user=config.USER,
+                                password=config.PASSWORD)
         cur = conn.cursor()
         create_tables = Default().commands
         for command in create_tables:
