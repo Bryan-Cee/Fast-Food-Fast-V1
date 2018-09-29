34606b24445431b61af4a9f2ed5991b4de002bd4
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-29 17:37:53 +0300

    [ Chore #160870252 ] Update configurations

--- instance/config.py
+++ instance/config.py
@@ -1,2 +1,35 @@
-DEBUG = False
-TESTING = False
+import os
+
+
+class Default:
+    """Base configuration."""
+    DEBUG = False
+    DBNAME = os.getenv('DBNAME')
+    PASSWORD = os.getenv('PASSWORD')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key')
+
+
+class DevelopmentConfig(Default):
+    """Development configuration."""
+    DEBUG = True
+
+
+class TestingConfig(Default):
+    """Testing configuration."""
+    DEBUG = True
+    TESTING = True
+    DBNAME = os.getenv('TESTDBNAME')
+
+
+class ProductionConfig(Default):
+    """Production configuration."""
+    DEBUG = False
+    DBNAME = os.getenv('DBNAME')
+    SECRET_KEY = 'secret_key'
+
+
+app_configs = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig
+}
