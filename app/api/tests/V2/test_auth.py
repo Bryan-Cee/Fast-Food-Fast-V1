e83fc4160ac8d76be83f5d7535bb83769d9a123c
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-09-30 11:43:46 +0300

    [ Chore #160790504 ] Update tests
    Refactored the test for creating an account

--- app/api/tests/V2/test_auth.py
+++ app/api/tests/V2/test_auth.py
@@ -21,31 +21,23 @@ class TestAuth(MainTestCase):
 
 	def test_login_wrong_password(self):
 		self.client.post('/api/v2/auth/signup', json={'username': 'BryanCee', 'password': '123456'})
-		h = Headers()
 		user = base64.b64encode(bytes('BryanCee:12345', 'UTF-8')).decode('UTF-8')
-		h.add('Authorization', 'Basic ' + user)
-		res = self.client.post('/api/v2/auth/login', headers=h)
+		res = self.client.post('/api/v2/auth/login', headers={'Authorization': 'Basic '+ user})
 		self.assertEqual(b'wrong password', res.data)
 
 	def test_login_no_authorization(self):
 		self.client.post('/api/v2/auth/signup', json={'username': 'BryanCee', 'password': '123456'})
-		h = Headers()
 		user = base64.b64encode(bytes('BryanCee:', 'UTF-8')).decode('UTF-8')
-		h.add('Authorization', 'Basic ' + user)
-		res = self.client.post('/api/v2/auth/login', headers=h)
+		res = self.client.post('/api/v2/auth/login', headers={'Authorization': 'Basic ' + user})
 		self.assertEqual(b'Could not verify, please input all your credentials', res.data)
 
 	def test_no_user(self):
-		h = Headers()
 		user = base64.b64encode(bytes('BryanCee:123456', 'UTF-8')).decode('UTF-8')
-		h.add('Authorization', 'Basic ' + user)
-		res = self.client.post('/api/v2/auth/login', headers=h)
+		res = self.client.post('/api/v2/auth/login', headers={'Authorization': 'Basic ' + user})
 		self.assertEqual(b'Could not verify, please check your username or password', res.data)
 
 	def test_login(self):
 		self.client.post('/api/v2/auth/signup', json={'username': 'BryanCee', 'password': '123456'})
-		h = Headers()
 		user = base64.b64encode(bytes('BryanCee:123456', 'UTF-8')).decode('UTF-8')
-		h.add('Authorization', 'Basic ' + user)
-		res = self.client.post('/api/v2/auth/login', headers=h)
+		res = self.client.post('/api/v2/auth/login', headers={'Authorization': 'Basic ' + user})
 		self.assertIn(b'Token', res.data)
