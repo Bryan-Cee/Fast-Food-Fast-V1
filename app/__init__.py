a02da129748c00f1484ce4c15976b933efebe57a
Cee Bryan <cheruiyotbryan@gmail.com>
2018-10-02 10:05:53 +0300

    Revert "#160336877 Add meal menu "

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,12 @@
 """__init__"""
 from flask import Flask
-import os
-
-from app.api.V2.database import InitDB
-from instance.config import app_configs
 
 
 def create_app():
     """Starts the application"""
    
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_configs[os.getenv('APP_SETTINGS')])
-    InitDB(app.config).create_tables()
-    from app.api.V2.Admin.views import admin_bp
-    app.register_blueprint(admin_bp)
+    app.config.from_pyfile('config.py')
     from app.api.V1.views import V1
     app.register_blueprint(V1)
-
     return app
