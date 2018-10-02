c8c27fce2cf8fcda904659439c9a9efd97186c10
Cee Bryan <cheruiyotbryan@gmail.com>
2018-10-02 12:32:19 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,15 @@
 """__init__"""
 from flask import Flask
 
+from instance.config import app_configs
 
 def create_app():
     """Starts the application"""
    
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.py')
+    app.config.from_object(app_configs[os.getenv('APP_SETTINGS')])
+    
     from app.api.V1.views import V1
     app.register_blueprint(V1)
+    
     return app
