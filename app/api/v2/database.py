c50e9d426899bf029382be0cf38925f93bf13ecd
Cee_Bryan <cheruiyotbryan@gmail.com>
2018-10-04 21:01:31 +0300

    [Chore #160950885] Implementing feedback

--- app/api/v2/database.py
+++ app/api/v2/database.py
@@ -7,7 +7,6 @@ class Default:
     """Initialize the tables in the database"""
     commands = (
         """	
-        DROP TABLE IF EXISTS Users CASCADE;
         CREATE TABLE IF NOT EXISTS Users (
             user_id serial NOT NULL PRIMARY KEY,
             username varchar(45) NOT NULL,
@@ -18,37 +17,26 @@ class Default:
         """
         ,
         """
-        DROP TABLE IF EXISTS Orders CASCADE;
-        CREATE TABLE IF NOT EXISTS  Orders (
-            order_id serial NOT NULL PRIMARY KEY,
-            meal_id int NOT NULL,
-            time_of_order timestamp NOT NULL,
-            user_id int NOT NULL,
-            order_status varchar(20) DEFAULT 'new' NOT NULL,
-            quantity int NOT NULL
-        );
-        """
-        ,
-        """
-        DROP TABLE IF EXISTS Menu CASCADE;
         CREATE TABLE IF NOT EXISTS Menu (
             meal_id serial PRIMARY KEY NOT NULL,
             meal_name varchar(45) NOT NULL,
             meal_desc varchar(100),
             meal_price float NOT NULL
         );
+        
         """
         ,
         """
-        ALTER TABLE Orders
-        ADD CONSTRAINT orders_users_user_id_fk
-        FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE ON UPDATE CASCADE;
-        """
-        ,
-        """
-        ALTER TABLE Orders
-        ADD CONSTRAINT orders_menu_meal_id_fk
-        FOREIGN KEY (meal_id) REFERENCES Menu (meal_id) ON DELETE CASCADE ON UPDATE CASCADE;
+        CREATE TABLE IF NOT EXISTS  Orders (
+            order_id serial NOT NULL PRIMARY KEY,
+            meal_id int NOT NULL,
+            time_of_order timestamp NOT NULL,
+            user_id int NOT NULL,
+            order_status varchar(20) DEFAULT 'new' NOT NULL,
+            quantity int NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY (meal_id) REFERENCES Menu (meal_id) ON DELETE CASCADE ON UPDATE CASCADE
+        );
         """
     )
 
